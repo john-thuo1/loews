f271ac20fe85bbbbf717f165a94458f3b8c1f6fd
johnthuo <j.mwangi@alustudent.com>
2024-01-19 15:43:07 +0200

    Modify Form

--- coreapp/forms.py
+++ coreapp/forms.py
@@ -5,7 +5,7 @@ class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
         fields = ['name', 'phone_number', 'report_date', 'species', 'stage', 'size', 'distribution', 
-                  'image', 'location','season','vegetation_details', 'gps_coordinates']
+                  'image', 'location','season','soil_type','vegetation_details', 'gps_coordinates']
         widgets = {
             'report_date': forms.DateInput(attrs={'type': 'date'}),
         }
