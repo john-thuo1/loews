d65699716df9b568c9ec603d8413750a2f6db19b
john-thuo1 <johmatrixthuo@gmail.com>
2024-06-11 01:52:47 +0300

    Revert views

--- coreapp/graph_utils.py
+++ coreapp/graph_utils.py
@@ -6,72 +6,81 @@ import json
 import pandas as pd
 import plotly.graph_objects as go
 from plotly.offline import plot
-from logger import setup_logger
+from coreapp.logger import setup_logger
+
 
 # Setup logger for this module
 logger = setup_logger(__name__, 'graph_utils.log')
 
 def plot_trend():
-    logger.info("Plotting trend")
     try:
+        logger.info("Plotting trend")
         df = pd.read_csv("Datasets/data.csv", parse_dates=["Report_Date"])
         df['Report_Date'] = pd.to_datetime(df['Report_Date'])
         df = df[df['Report_Date'].dt.year <= 2023]
-        monthly_tally = df.resample('M', on='Report_Date')['Land_Size(Acres)'].sum().reset_index()
-        
+
+        monthly_tally = df.resample('ME', on='Report_Date')['Land_Size(Acres)'].sum().reset_index()
+
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=monthly_tally['Report_Date'], y=monthly_tally['Land_Size(Acres)'], mode='lines+markers', marker=dict(size=10),
-                                name='Affected Monthly Land Size in Acres For both Infestation and Breeding Grounds', showlegend=False))
-        
+                                 name='Affected Monthly Land Size in Acres For both Infestation and Breeding Grounds', showlegend=False))
+
         # Adding Prophet predictions
         prophet_pred = plot_prophet_predictions(monthly_tally)
         fig.add_trace(go.Scatter(x=prophet_pred['ds'], y=prophet_pred['yhat'], mode='lines', line=dict(dash='dot'), name='Prophet Predictions', showlegend=False))
-        
+
         # Adding vertical line for future predictions
+        # Note : Error while using fig.add_vline on Plotly (unsupported operand type(s) for +: 'int' and 'datetime.datetime')
         future_date = pd.to_datetime('2023-12-31')
         future_date1 = pd.to_datetime('2024-01-01')
         fig.add_vrect(x0=future_date, x1=future_date1, line_dash="dash", line_color="red")
-        
+
         fig.update_layout(title='Land Size Affected by Locusts',
-                        xaxis_title='Year',
-                        yaxis_title='Land Size (Acres)',
-                        title_x=0.5)
-        
+                          xaxis_title='Year',
+                          yaxis_title='Land Size (Acres)',
+                          title_x=0.5)
+
         trends_html = plot(fig, output_type='div')
         return trends_html
     except Exception as e:
         logger.error(f"Error in plot_trend: {e}")
+        return None
 
 
 def plot_prophet_predictions(data):
-    logger.info("Generating Prophet predictions")
     try:
+        logger.info("Plotting Prophet predictions")
         prophet_df = data.rename(columns={'Report_Date': 'ds', 'Land_Size(Acres)': 'y'})
         model = Prophet()
         model.fit(prophet_df)
-        
+
         # Forecasting for 12 months into the future
         future = model.make_future_dataframe(periods=12, freq='M')
         forecast = model.predict(future)
         return forecast[['ds', 'yhat']]
     except Exception as e:
         logger.error(f"Error in plot_prophet_predictions: {e}")
+        return None
 
 
 def plot_regions():
-    logger.info("Plotting regions")
     try:
-        df = pd.read_csv('../Datasets/data.csv')
+        logger.info("Plotting regions")
+        df = pd.read_csv('Datasets/data.csv')
+
         df['Stage'] = df['Stage'].astype(str)
+
         df['Category'] = pd.NA
         df.loc[df['Stage'].isin(['Hoppers/Nymph(Breeding Ground)', 'Eggs(Breeding Ground)']), 'Category'] = 'Breeding Ground'
         df.loc[df['Stage'].isin(['Fledglings(Young Adults)', 'Adults']), 'Category'] = 'Infestation'
         df.loc[df['Stage'] == 'Unknown', 'Category'] = 'Unconfirmed'
-        
+
         grouped_df = df.groupby(['Location', 'Category']).size().reset_index(name='Count')
+
         pivot_df = grouped_df.pivot(index='Location', columns='Category', values='Count').reset_index()
+
         sorted_df = pivot_df.sort_values(by='Infestation', ascending=False).head(209)
-        
+
         table_html = """
         <div class="table-responsive">
             <table class="table table-hover">
@@ -96,34 +105,39 @@ def plot_regions():
                     <td>{row['Unconfirmed']}</td>
                 </tr>
             """
-        
+
         table_html += """
                 </tbody>
             </table>
         </div>
         """
-        
+
         return table_html, df
     except Exception as e:
         logger.error(f"Error in plot_regions: {e}")
-        
+        return None, None
+
 
 def plot_seasonality():
-    logger.info("Plotting seasonality")
     try:
+        logger.info("Plotting seasonality")
         _, df = plot_regions()
+
         infestation_df = df[df['Category'] == 'Infestation']
         breeding_ground_df = df[df['Category'] == 'Breeding Ground']
-        
+
         # Group by Season and calculate counts
         infestation_counts = infestation_df.groupby('Season').size().reset_index(name='Infestation Count')
         breeding_ground_counts = breeding_ground_df.groupby('Season').size().reset_index(name='Breeding Ground Count')
-        
+
         fig2 = go.Figure()
+
         fig2.add_trace(go.Bar(x=infestation_counts['Season'], y=infestation_counts['Infestation Count'],
                             name='Infestations', marker_color='skyblue', width=0.2)) 
+
         fig2.add_trace(go.Bar(x=breeding_ground_counts['Season'], y=breeding_ground_counts['Breeding Ground Count'],
                             name='Breeding Grounds', marker_color='salmon', width=0.2)) 
+
         fig2.update_layout(title='',
                         xaxis_title='',
                         yaxis_title='Count',
@@ -132,20 +146,26 @@ def plot_seasonality():
                         margin=dict(l=0, r=0, t=30, b=0))
         fig2.update_xaxes(tickvals=[], ticktext=[])
         fig2.update_yaxes(range=[0, 900]) 
-        
+
+
         season_html = plot(fig2,  output_type='div')
+
+
         return season_html
     except Exception as e:
         logger.error(f"Error in plot_seasonality: {e}")
-        
+        return None
 
+    
 def plot_vegetation():
-    logger.info("Plotting vegetation")
     try:
+        logger.info("Plotting vegetation")
         _, df = plot_regions()
+
         count_by_category = df.groupby(['Vegetation_Details', 'Category']).size().unstack(fill_value=0)
-        
+
         fig3 = go.Figure()
+
         for category in count_by_category.columns:
             fig3.add_trace(go.Bar(
                 x=count_by_category.index,
@@ -153,7 +173,7 @@ def plot_vegetation():
                 name=f'{category}',
                 width=0.3,
             ))
-        
+
         fig3.update_layout(
             title='',
             xaxis=dict(title=''),
@@ -162,23 +182,26 @@ def plot_vegetation():
             margin=dict(l=0, r=0, t=30, b=0),
             bargap=0.1
         )
+
         fig3.update_yaxes(range=[0, 550]) 
         fig3.update_xaxes(tickvals=[], ticktext=[])
-        
+
         vegetation_html = plot(fig3,  output_type='div')
+        
         return vegetation_html
     except Exception as e:
         logger.error(f"Error in plot_vegetation: {e}")
-        
+        return None
+
 
 def plot_predictionmap():
-    logger.info("Plotting prediction map")
     try:
-        df = pd.read_csv('../Datasets/predictions.csv', encoding='utf-8')
-        geojson_path = '../Datasets/kenya_vector_boundaries1.geojson'
+        logger.info("Plotting prediction map")
+        df = pd.read_csv('Datasets/predictions.csv', encoding='utf-8')
+        geojson_path = 'Datasets/kenya_vector_boundaries1.geojson'
         with open(geojson_path, 'r', encoding='utf-8') as f:
             geojson_data = json.load(f)
-        
+
         prediction_map = folium.Map(location=[-0.0236, 37.9062], zoom_start=6)
         marker_cluster = MarkerCluster().add_to(prediction_map)
         for i in range(len(df)):
@@ -191,20 +214,20 @@ def plot_predictionmap():
                     location=[lat, lon],
                     popup=folium.Popup(popup_content,  max_width=800, max_height=450)
                 ).add_to(marker_cluster)
-        
+
         if geojson_path:
             folium.GeoJson(
                 geojson_data,
                 name='geojson',
-                style_function=lambda x: {
-                'fillColor': '#0000ff',
-                'color': '#0000ff',
-                'weight': 1,
-                'fillOpacity': 0
-            },
+                 style_function=lambda x: {
+                  'fillColor': '#0000ff',
+                  'color': '#0000ff',
+                  'weight': 1,
+                  'fillOpacity': 0
+              },
             ).add_to(prediction_map)
-        
+
         return prediction_map
     except Exception as e:
         logger.error(f"Error in plot_predictionmap: {e}")
-        
+        return None
--- coreapp/views.py
+++ coreapp/views.py
@@ -4,7 +4,7 @@ import re
 import os
 
 # Third-party Library Imports
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, HttpResponseServerError, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 from django.utils import timezone
@@ -14,92 +14,94 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from pypdf import PdfReader
-from typing import Any
-from logger import setup_logger
-
-
-
+from coreapp.logger import setup_logger
 
 # Local Imports
 from .forms import ReportForm
 from .graph_utils import (plot_trend, plot_regions, plot_seasonality, plot_vegetation, plot_predictionmap)
-from .models import Report
+from .models import Report, Chat
 from openai import OpenAI
-from .models import Chat
 
 
 load_dotenv() 
 
-user_api_key= os.getenv('OPENAI_API_KEY')
+user_api_key = os.getenv('OPENAI_API_KEY')
 
-client = OpenAI(
-    api_key= user_api_key
-)
+client = OpenAI(api_key=user_api_key)
 
 logger = setup_logger(__name__, 'views.log')
 
 
-
 def control_mitigation(request):
-    logger.info("Rendering control_mitigation page")
-    return render(request, "coreapp/mitigation.html")
+    try:
+        logger.info("Accessing mitigation control")
+        return render(request, "coreapp/mitigation.html")
+    except Exception as e:
+        logger.error(f"Error in control_mitigation: {e}")
+        return HttpResponseServerError("An error occurred while accessing mitigation control.")
 
 
 def self_report(request):
-    logger.info("Rendering self_report page")
-    return render(request, "coreapp/self_report.html")
+    try:
+        logger.info("Accessing self report")
+        return render(request, "coreapp/self_report.html")
+    except Exception as e:
+        logger.error(f"Error in self_report: {e}")
+        return HttpResponseServerError("An error occurred while accessing self report.")
 
 
 def dashboard(request):
-    logger.info("Rendering dashboard")
     try:
+        logger.info("Accessing dashboard")
         trends_html = plot_trend()
         table_html = plot_regions()[0]
         season_html = plot_seasonality()
         vegetation_html = plot_vegetation()
         context = {'trends_html': trends_html, 'table_html': table_html, 
                    'season_html': season_html, 'vegetation_html': vegetation_html}
-        logger.info("Dashboard context prepared successfully")
+        return render(request, "coreapp/dashboard.html", context)
     except Exception as e:
         logger.error(f"Error in dashboard: {e}")
-    return render(request, "coreapp/dashboard.html", context)
+        return HttpResponseServerError("An error occurred while accessing dashboard.")
 
 
 def load_data():
-    pdf_path = "../Datasets/pesticides.pdf"
-    if not os.path.exists(pdf_path):
-        logger.error(f"PDF file '{pdf_path}' not found.")
-        return ""
-    
-    text = ""
     try:
+        logger.info("Loading data")
+        pdf_path = "Datasets/pesticides.pdf"
+        if not os.path.exists(pdf_path):
+            logger.error(f"PDF file '{pdf_path}' not found.")
+            return ""
+        
+        text = ""
         with open(pdf_path, "rb") as file:
             pdf_reader = PdfReader(file)
             for page in pdf_reader.pages:
                 text += page.extract_text()
-        logger.info("PDF data loaded successfully")
+        return text
     except Exception as e:
-        logger.error(f"Error while reading PDF: {e}")
-    
-    return text
+        logger.error(f"Error in load_data: {e}")
+        return ""
 
 
-def process_text(text: str) -> FAISS:
+def process_text(text):
     try:
+        logger.info("Processing text")
         text_splitter = CharacterTextSplitter(separator="\n", keep_separator=True, chunk_size=1000, 
                                               chunk_overlap=200, length_function=len)
         chunks = text_splitter.split_text(text)
         
         embeddings = OpenAIEmbeddings(api_key=os.getenv('OPENAI_API_KEY'))
         knowledgeBase = FAISS.from_texts(chunks, embeddings)
-        logger.info("Text processed successfully")
         return knowledgeBase
     except Exception as e:
         logger.error(f"Error in process_text: {e}")
+        return None
 
 
-def query_chat(message: str, similar_documents: str| Any) -> str| None:
+def query_chat(message, similar_documents):
     try:
+        logger.info("Querying chat")
         similar_documents = [str(doc_id) for doc_id in similar_documents]
 
         response = client.chat.completions.create(
@@ -108,17 +110,19 @@ def query_chat(message: str, similar_documents: str| Any) -> str| None:
                 {"role": "system", "content": "You are a locust outbreak mitigation expert. If you do not know the answer, or are unsure, say you don't know."},
                 {"role": "user", "content": message},
                 {"role": "system", "content": "Similar documents: \n" + "\n".join(similar_documents)}
+            
             ]
         )
         answer = response.choices[0].message.content.strip()
-        logger.info("Query chat completed successfully")
         return answer
     except Exception as e:
         logger.error(f"Error in query_chat: {e}")
+        return "Sorry, an error occurred while processing your request."
 
 
 def format_response(response):
     try:
+        logger.info("Formatting response")
         lines = response.split('\n')
         formatted_lines = []
         for line in lines:
@@ -132,33 +136,39 @@ def format_response(response):
                     formatted_lines.append(f'<p>{line.strip()}</p>')
                 else:
                     formatted_lines.append(f'<p>{line.strip()}</p>')
-        logger.info("Response formatted successfully")
         return ''.join(formatted_lines)
     except Exception as e:
         logger.error(f"Error in format_response: {e}")
-        
+        return "Sorry, an error occurred while processing your request."
 
 
-def rag_chat(request):
+def rag_chat(request): 
     try:
+        logger.info("Chatting")
         chats = Chat.objects.filter(user=request.user)
         if request.method == "POST":
+            # User Query
             message = request.POST.get("message")
+            
             document_text = load_data()
             knowledge_base = process_text(document_text)
-            similar_documents = knowledge_base.similarity_search(message)
-            response = query_chat(message, similar_documents)
-            chat = Chat(user=request.user, message=message, response=response, created_at=timezone.now())
-            chat.save()
-            logger.info("RAG chat processed and saved successfully")
-            return JsonResponse({"message": message, "response": format_response(response)})
+            if knowledge_base:
+                similar_documents = knowledge_base.similarity_search(message)
+                response = query_chat(message, similar_documents)
+                chat = Chat(user=request.user, message=message, response=response, created_at=timezone.now())
+                chat.save()
+                return JsonResponse({"message": message, "response": format_response(response)})
+            else:
+                return JsonResponse({"error": "An error occurred while processing the document."})
         return render(request, "coreapp/chat.html", {"chats": chats})
     except Exception as e:
         logger.error(f"Error in rag_chat: {e}")
+        return HttpResponseServerError("An error occurred while accessing the chat.")
 
 
 def download_data(request):
     try:
+        logger.info("Downloading data")
         reports_data = Report.objects.all().values()
         columns_to_drop = ['name', 'phone_number']
         response = HttpResponse(content_type='text/csv')
@@ -167,38 +177,53 @@ def download_data(request):
         header_row = [field for field in reports_data[0] if field not in columns_to_drop]
         csv_writer.writerow(header_row)
 
+        # Write the data rows excluding specified columns
         for report in reports_data:
             cleaned_report = [report[field] for field in header_row]
             csv_writer.writerow(cleaned_report)
-        logger.info("Data downloaded successfully")
         return response
     except Exception as e:
         logger.error(f"Error in download_data: {e}")
-    
-    
+        return HttpResponseServerError("An error occurred while downloading data.")
+
+
 def delete_chats(request):
-    if request.method == "DELETE":
-        Chat.objects.filter(user=request.user).delete()
-        return JsonResponse({"message": "Chats deleted successfully"})
-    else:
-        return JsonResponse({"error": "Invalid method. Use DELETE."})
+    try:
+        logger.info("Deleting chats")
+        if request.method == "DELETE":
+            Chat.objects.filter(user=request.user).delete()
+            return JsonResponse({"message": "Chats deleted successfully"})
+        else:
+            return JsonResponse({"error": "Invalid method. Use DELETE."})
+    except Exception as e:
+        logger.error(f"Error in delete_chats {e}")
+
        
 
 def map_predictions(request):
-    map_html = plot_predictionmap()
-    context = {'map_html': map_html._repr_html_()}
-    return  render(request, "coreapp/index.html", context)
+    try:
+        logger.info("Generating prediction map")
+        map_html = plot_predictionmap()
+        context = {'map_html': map_html._repr_html_()}
+        return render(request, "coreapp/index.html", context)
+    except Exception as e:
+        logger.error(f"Error in map_predictions: {e}")
+        return HttpResponseServerError("An error occurred while generating prediction map.")
 
 
 def dashboard(request):
-    trends_html = plot_trend()
-    table_html = plot_regions()[0]
-    season_html = plot_seasonality()
-    vegetation_html = plot_vegetation()
-    context = {'trends_html': trends_html, 'table_html': table_html, 
-               'season_html': season_html, 'vegetation_html': vegetation_html}
-    
-    return render(request, "coreapp/dashboard.html", context)
+    try:
+        logger.info("Generating dashboard")
+        trends_html = plot_trend()
+        table_html = plot_regions()[0]
+        season_html = plot_seasonality()
+        vegetation_html = plot_vegetation()
+        context = {'trends_html': trends_html, 'table_html': table_html, 
+                   'season_html': season_html, 'vegetation_html': vegetation_html}
+        return render(request, "coreapp/dashboard.html", context)
+    except Exception as e:
+        logger.error(f"Error in dashboard: {e}")
+        return HttpResponseServerError("An error occurred while generating dashboard.")
 
 
 class SelfReportCreateView(CreateView):
